c72a53005ff74a3d72aba0439f22bcce77f3ce1c
Suisuroru <qwertyuiop14077@qq.com>
2025-01-24 19:50:41 +0800

    style:优化 HTML 和 JavaScript代码格式
    - 使用一致的空格和缩进
    - 修复 meta 标签格式问题
    - 添加 allow 属性到 iframe
    - 优化 JavaScript 对象定义

--- resources/js/public/menu.js
+++ resources/js/public/menu.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const navItems = [
-        { text: '首页', href: 'index.html' },
-        { text: '关于我们', href: 'about-us.html' },
-        { text: '状态信息', href: 'https://stats.blue-millennium.fun', target: '_blank' },
-        { text: '轨交线路图', href: 'map.html', target: '_blank' },
-        { text: '加入我们', href: 'https://qm.qq.com/cgi-bin/qm/qr?k=N8TldcldaRkQXGFlXc_mTeYxqMnJ95es&jump_from=webapi&authKey=9rsk7N8LLGLdzwkoAO/qCZmPc4JaOfGbcGRfjFWgmkAdC1CbfDe7qifhu4FXtCf7', target: '_blank' }
+        {text: '首页', href: 'index.html'},
+        {text: '关于我们', href: 'about-us.html'},
+        {text: '状态信息', href: 'https://stats.blue-millennium.fun', target: '_blank'},
+        {text: '轨交线路图', href: 'map.html', target: '_blank'},
+        {
+            text: '加入我们',
+            href: 'https://qm.qq.com/cgi-bin/qm/qr?k=N8TldcldaRkQXGFlXc_mTeYxqMnJ95es&jump_from=webapi&authKey=9rsk7N8LLGLdzwkoAO/qCZmPc4JaOfGbcGRfjFWgmkAdC1CbfDe7qifhu4FXtCf7',
+            target: '_blank'
+        }
     ];
 
     const navList = document.getElementById('nav-list');
